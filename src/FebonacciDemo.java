cfe5b0cccc9bffb8a19f9e8f464e4a7a216b8cf5
Kum@r123 <subodh.msu@gmail.com>
2025-03-29 01:21:32 +0530

    fibonacci number

---
+++ src/FebonacciDemo.java
@@ -0,0 +1,14 @@
+public class FebonacciDemo {
+
+    public static int fibonacci(int n){
+        if(n==0 || n==1)
+            return n;
+        else {
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fibonacci(4));
+    }
+}
